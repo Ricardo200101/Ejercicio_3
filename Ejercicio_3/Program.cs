06354d5053f369a0021aff9655f186eb41db2acb
Ricardo Alvarez <SMIS937420@ugb.edu.sv>
2021-07-28 10:27:57 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variable. 
+
+            int num1, num2, divi;
+
+            // Captura de datos. 
+
+            Console.WriteLine("INGRESAR EL PRIMER DIGITO: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("INGRESAR EL SEGUNDO DIGITO: ");
+            num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            // Condicion 
+
+            if (num2 <= 0)
+            {
+                Console.WriteLine("ERROR: NO SE PUEDE DIVIDIR ENTRE 0");
+            }
+            else
+            {
+                divi = num1 / num2;
+
+                // Mostrar en patalla. 
+
+                Console.WriteLine("LA DIVISION DEL PRIMER DIGITO ENTRE SEGUNDO DIGITO ES: {0}", divi); 
+
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
